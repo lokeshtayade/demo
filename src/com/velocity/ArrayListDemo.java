524f89526ea0ea26ab70ada1af029860392c16f8
lokeshtayade <taydelokesh123@gmail.com>
2022-12-24 23:16:02 +0530

    added Linkedlist demo

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("Abhi");
+		arrayList.add("Morshkar");
+		System.out.println(arrayList);
+	}
+}
