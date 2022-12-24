518ddcff2faedf1d502bcfcd2310145ddcb689ef
lokeshtayade <taydelokesh123@gmail.com>
2022-12-24 23:06:13 +0530

    added LinkedList demo code

---
+++ src/com/velocity/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList<String> linkedList = new LinkedList<String>();
+		linkedList.add("lokesh");
+		linkedList.add("Bhagwan");
+		linkedList.add("Tayade");
+		System.out.println(linkedList);
+	}
+
+}
